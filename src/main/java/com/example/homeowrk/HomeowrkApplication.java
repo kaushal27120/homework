86b6faf0acc7361e1f02f616d96f6ad889d8be63
Kaushal <jasanikaushal@yahoo.com>
2023-12-15 17:59:55 +0100

    homework

---
+++ src/main/java/com/example/homeowrk/HomeowrkApplication.java
@@ -0,0 +1,13 @@
+package com.example.homeowrk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeowrkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeowrkApplication.class, args);
+	}
+
+}
